5e5b5520a330bc9e7ea317f32eab820da383d968
RAYQUXA <140433509+kenryujugie@users.noreply.github.com>
2026-08-26 11:25:43 +0530

    Add files via upload

---
+++ day3_code1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    float celsius;
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &celsius);
+
+    float fahrenheit = (celsius * 9 / 5) + 32;
+    printf("Temperature in Fahrenheit: %.2f\n", fahrenheit);
+    
+    return 0;
+
+}
