ace9dd83ef7f53b545ac2a9708f0588242aa61ca
RAYQUXA <140433509+kenryujugie@users.noreply.github.com>
2026-08-26 11:51:14 +0530

    Add files via upload

---
+++ day4_code2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum = sum + i;
+    }
+    printf("Sum of first %d natural numbers: %d\n", n, sum);
+
+    return 0;
+}
