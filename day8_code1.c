06d50ce9dae203bb3ff93b7a442a00d8604b3f4e
RAYQUXA <140433509+kenryujugie@users.noreply.github.com>
2026-08-28 20:21:20 +0530

    Add files via upload

---
+++ day8_code1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+    char ch;
+    printf("Enter a alphabet : ");
+    scanf("%c", &ch);
+
+    if (ch >= 'a' && ch <= 'z') {
+        printf("%c is an lowercase alphabet\n", ch);
+    }
+    else if (ch >= 'A' && ch <= 'Z') {
+        printf("%c is an UPPERCSE alphabet\n", ch);
+    }
+    else if (ch >= '0' && ch <= '9') {
+        printf("%c is a digit\n", ch);
+    }
+    else {
+        printf("%c is a special character\n", ch);
+    }
+    return 0;
+
+}
