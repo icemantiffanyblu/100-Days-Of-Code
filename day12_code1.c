92407bfee563a4edb3578cc891cdaa3f40e5d087
RAYQUXA <140433509+icemantiffanyblu@users.noreply.github.com>
2026-09-05 00:15:08 +0530

    Add files via upload

---
+++ day12_code1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int days;
+    float fine = 0;
+    
+    printf("Enter number of late days: ");
+    scanf("%d", &days);
+    
+    if (days > 30) {
+        printf("Membership Cancelled!\n");
+    }
+    else if (days > 10) {
+        fine = (5 * 2) + (5 * 4) + ((days - 10) * 6);
+        printf("Fine: Rs. %.2f\n", fine);
+    }
+    else if (days > 5) {
+        fine = (5 * 2) + ((days - 5) * 4);
+        printf("Fine: Rs. %.2f\n", fine);
+    }
+    else {
+        fine = days * 2;
+        printf("Fine: Rs. %.2f\n", fine);
+    }
+    
+    return 0;
+}
